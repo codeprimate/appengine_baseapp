510ad5e5e3465e009baeb38f2f5bff6c7c9def6b
Patrick Morgan <patrick.morgan@gmail.com>
2008-12-21 22:27:59 -0600

    initial commit

---
+++ controller.py
@@ -0,0 +1,11 @@
+import os
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+
+class BaseController(webapp.RequestHandler):
+	def render_template(self, view, values):
+		self.response.out.write(template.render(self.path_for_template(view), values))
+
+	def path_for_template( self, view):
+		path = os.path.join(os.path.dirname(__file__), 'views/' + view)
+		return(path)
